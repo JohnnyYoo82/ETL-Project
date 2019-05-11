cdc6a65cc893380774311de617f2574975cbedfc
John Yoo <johnnyyoo82@gmail.com>
2019-05-11 11:47:42 -0500

    updating ETL Project

--- query.sql
+++ query.sql
@@ -1,10 +1,13 @@
+-- Use country_db
+USE country_db;
+
 -- Query to check successful load
 SELECT * FROM countrydata;
 
-SELECT * FROM county;
+SELECT * FROM costofliving;
 
--- Join tables on county_id
-SELECT countrydata.country, countrydata.region, countrydata.population_inthousands, countrydata.gdp_inmillions, countrydata.urbanpop_pcttotalpop,
+-- Join tables on county
+SELECT countrydata.country, countrydata.region, countrydata.population_inthousands, countrydata.gdp_inmillions, countrydata.urbanpop_pcttotalpop, costofliving.country, costofliving.costofliving, costofliving.groceries_index, costofliving.restaurant_index
 FROM countrydata
-INNER JOIN county
-ON countrydata.country = county.country;
+INNER JOIN costofliving
+ON countrydata.country = costofliving.country;
